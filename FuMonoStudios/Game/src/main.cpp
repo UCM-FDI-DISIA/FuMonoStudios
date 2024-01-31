753191f71c5447c1d114bf45ab543743e5db0975
LuisEnriqueCabelloBlanco <luisecab@ucm.es>
2024-02-01 00:39:44 +0100

    Commit de prueba

--- FuMonoStudios/Game/src/main.cpp
+++ FuMonoStudios/Game/src/main.cpp
@@ -4,6 +4,7 @@
 #include <SDL_image.h>
 
 int main(int argc, char* argv[]) {
+	std::cout << "Hola Buenas Tardes";
 	SDL_Init(SDL_INIT_EVERYTHING);
 	
 	SDL_Window* window = SDL_CreateWindow("The Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,800, 600,SDL_WINDOW_RESIZABLE);
