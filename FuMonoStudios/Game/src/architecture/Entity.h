9c83704ad428bbf36610790529e560ced884d8b7
PablooMS <pamarc05@ucm.es>
2024-02-05 16:49:20 +0100

    ECS my beloved
    Comenzado con el Entity-Component

---
+++ FuMonoStudios/Game/src/architecture/Entity.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "ecs.h"
+
+namespace ecs {
+	class Entity
+	{
+	public:
+		Entity(Manager* mng);
+		virtual ~Entity();
+		inline bool isAlive() const;
+		inline void setAlive();
+		Manager* getMngr() const;
+
+		template<typename T, typename ...Ts>
+		inline T* addComponent(cmpId_t cId, Ts&&... args) {
+			T* c = new T(std::forward<Ts>(args)…);
+
+			removeComponent(cId);
+
+			currCmps_.push_back(c);
+			cmps_[cId] = c;
+
+			c->setContext(this);
+			c->initComponent();
+
+			return c;
+		}
+
+		inline void removeComponent(cmpId_t cId) {
+			if (cmps_[cId] != nullptr) {
+				auto iter = std::find(currCmps_.begin(),
+					currCmps_.end(),
+					cmps_[cId]);
+				currCmps_.erase(iter);
+				delete cmps_[cId];
+				cmps_[cId] = nullptr;
+			}
+		}
+
+		template<typename T>
+		inline T* getComponent(ecs::cmpId_t cId) {
+			return static_cast<T*>(cmps_[cId]);
+		}
+		inline bool hasComponent(cmpId_t cId) {
+			return cmps_[cId] != nullptr;
+		}
+
+	private:
+		bool alive_;
+		Manager* mngr_;
+		std::vector<Component*> currCmps_;
+		std::array<Component*, ecs::maxComponentId> cmps_;
+	};
+
+}
+
---
+++ FuMonoStudios/Game/src/architecture/ecs.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <vector>
+
+namespace ecs {
+
+	using cmpId_t = uint8_t;
+	namespace cmp {
+		enum cmpId : cmpId_t {
+			TRANSFORM = 0,
+			IMAGE,
+			CTRL,
+			// do not remove this
+			_LAST_CMP_ID
+		};
+	}
+	constexpr cmpId_t maxComponentId = _LAST_CMP_ID;
+
+	class Component {
+	public:
+		Component() : ent_() {}
+		virtual ~Component() {}
+		inline void setContext(Entity* ent) {
+			ent_ = ent;
+		}
+
+		virtual void initComponent() {};
+		virtual void update() {};
+		virtual void render() {};
+	protected:
+		Entity* ent_;
+	};
+
+	class Manager {
+
+	};
+}
+
