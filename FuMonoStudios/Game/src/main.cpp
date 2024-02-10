57b92b5f4e01e4306d3911ecf645fa29152525f6
DanielZhu26 <125443437+DanielZhu26@users.noreply.github.com>
2024-02-10 18:43:21 +0100

    continuacion de los dialogos
    me salta el error con la carpeta sdl.mixer asi q sigo con otra plantilla mia

--- FuMonoStudios/Game/src/main.cpp
+++ FuMonoStudios/Game/src/main.cpp
@@ -1,36 +1,65 @@
-#include <iostream>
+
+
+#include <stdio.h>
+#include <stdint.h>
 #include <SDL.h>
 #include <SDL_ttf.h>
-#include <SDL_image.h>
-#include <stdio.h>
-#include <string.h>
-#include "sdlutils/SDLUtils.h"
 
+int main(int argc, char* argv[]) {
 
-//
-//
-//int main(int argc, char* argv[]) {
-//	std::cout << "Hola Buenas Tardes";
-//	//SDL_Init(SDL_INIT_EVERYTHING);
-//	//
-//	//SDL_Window* window = SDL_CreateWindow("The Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,800, 600,SDL_WINDOW_RESIZABLE);
-//	//SDL_Event evento;
-//
-// //   int SDL_ShowSimpleMessageBox(Uint32 flags, const char* title, const char* message, SDL_Window * window);
-//
-//	//bool quit = false;
-//
-//	//while (!quit) {
-//	//	while (SDL_PollEvent(&evento)) {
-//	//		if (evento.type == SDL_QUIT) {
-//	//			quit = true;
-//	//		}
-//	//	}
-//	//}
-//
-// 
-//
-//    return 0;
-//}
-//   
-//
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // create window and renderer
+
+    SDL_Window* Window = SDL_CreateWindow("Text Rendering", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 640, 0);
+    if (Window == NULL) { printf("Window creation failed: %s\n", SDL_GetError()); }
+    SDL_Renderer* Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (Renderer == NULL) { printf("Renderer creation failed\n: %s", SDL_GetError()); }
+
+    // create text texture
+
+    TTF_Init();
+
+    TTF_Font* Font = TTF_OpenFont("resources/fonts/ARIAL.ttf", 40);
+    if (Font == NULL) { printf("Font loading failed: %s\n", TTF_GetError()); }
+
+    SDL_Color textColor = { 255, 255, 255, 255 };
+
+    SDL_Surface* TextSurface = TTF_RenderText_Blended_Wrapped(Font, "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.", textColor, 500);
+    if (TextSurface == NULL) { printf("Surface creation failed: %s\n", TTF_GetError()); }
+
+    SDL_Texture* TextTexture = SDL_CreateTextureFromSurface(Renderer, TextSurface);
+    if (TextSurface == NULL) { printf("Texture creation failed: %s\n", TTF_GetError()); }
+
+    uint32_t Running = 1;
+
+    SDL_Event Event = { 0 };
+
+    while (Running) {
+        while (SDL_PollEvent(&Event)) {
+            switch (Event.type) {
+            case SDL_QUIT: { Running = 0; } break;
+            case SDL_KEYDOWN: {
+                switch (Event.key.keysym.sym) {
+                case SDLK_o: { Running = 0; } break;
+                }
+            } break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(Renderer, 30, 30, 30, 255);
+        SDL_RenderClear(Renderer);
+
+        // display text
+
+        SDL_Rect destRect = { 640 / 2 - TextSurface->w / 2, 640 / 2 - TextSurface->h / 2, TextSurface->w, TextSurface->h };
+        SDL_RenderCopy(Renderer, TextTexture, NULL, &destRect);
+
+        SDL_RenderPresent(Renderer);
+    }
+
+    SDL_Quit();
+
+    return 0;
+}
+
