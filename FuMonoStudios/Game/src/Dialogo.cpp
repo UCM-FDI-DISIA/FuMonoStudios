899645fd695dee7e77d7b6971a4ae8bd6703291c
DanielZhu26 <125443437+DanielZhu26@users.noreply.github.com>
2024-02-17 21:58:48 +0100

    ajustes del codigo de la clase dialogo
    falta convertirlo a componente que he estado toda la tarde y no me salia

--- FuMonoStudios/Game/src/Dialogo.cpp
+++ FuMonoStudios/Game/src/Dialogo.cpp
@@ -1,120 +1,128 @@
+// dialogo.cpp
+
 #include "dialogo.h"
+#include <iostream>
 
+Dialogo::Dialogo()
+    : window(nullptr), renderer(nullptr), font(nullptr),
+    textToWrite("Día 1: ..."), textLength(0),
+    displayedText(nullptr), TextSurface(nullptr), TextTexture(nullptr),
+    currentCharIndex(0), delayCounter(0), delayAmount(50) {
+    InitializeSDL();
 
-Dialogo::Dialogo() {
-  InitializeSDL();
+    window = SDL_CreateWindow("Text Rendering", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 640, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-  window = SDL_CreateWindow("Text Rendering", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 640, 0);
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    TTF_Init();
+    font = TTF_OpenFont("resources/fonts/ARIAL.ttf", 20);
 
-  TTF_Init();
-  font = TTF_OpenFont("resources/fonts/ARIAL.ttf", 20);
+    textColor = { 255, 255, 255, 255 };
 
-  textColor = { 255, 255, 255, 255 };
+    while (textToWrite[textLength] != '\0') {
+        textLength++;
+    }
 
-  textToWrite = "Día 1: El vagabundo estará en su caja de cartón en la calle donde está la oficina. El juego te indicará que puedes hablar con él y te forzará a hablar con él.";
+    displayedText = std::make_unique<char[]>(textLength + 1);
+    displayedText[0] = '\0';
 
-  textLength = 0;
-  while (textToWrite[textLength] != '\0') {
-    textLength++;
-  }
+    TextSurface = nullptr;
+    TextTexture = nullptr;
 
-  displayedText = (char*)malloc(textLength + 1);
-  displayedText[0] = '\0';
-
-  TextSurface = NULL;
-  TextTexture = NULL;
-
-  textBoxRect = { 20, 480, 600, 120 };
-
-  currentCharIndex = 0;
-  delayCounter = 0;
-  delayAmount = 50;
+    textBoxRect = { 20, 480, 600, 120 };
 }
 
 Dialogo::~Dialogo() {
-  Clean();
+    Clean();
 }
 
 void Dialogo::InitializeSDL() {
-  SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+        exit(EXIT_FAILURE);
+    }
 }
 
 void Dialogo::CreateTextTexture() {
-  if (TextSurface != NULL) {
-    SDL_FreeSurface(TextSurface);
-  }
-
-  TextSurface = TTF_RenderText_Blended_Wrapped(font, displayedText, textColor, 500);
-
-  if (TextSurface != NULL && TextSurface->w > 0) {
-    if (TextTexture != NULL) {
-      SDL_DestroyTexture(TextTexture);
+    if (TextSurface != nullptr) {
+        SDL_FreeSurface(TextSurface);
     }
-    TextTexture = SDL_CreateTextureFromSurface(renderer, TextSurface);
-  }
+
+    TextSurface = TTF_RenderText_Blended_Wrapped(font, displayedText.get(), textColor, 500);
+
+    if (TextSurface != nullptr && TextSurface->w > 0) {
+        if (TextTexture != nullptr) {
+            SDL_DestroyTexture(TextTexture);
+        }
+        TextTexture = SDL_CreateTextureFromSurface(renderer, TextSurface);
+    }
 }
 
 void Dialogo::HandleEvents() {
-  SDL_Event event;
-  while (SDL_PollEvent(&event)) {
-    switch (event.type) {
-    case SDL_QUIT:
-      exit(0);
-      break;
-    case SDL_KEYDOWN:
-      switch (event.key.keysym.sym) {
-      case SDLK_o:
-        exit(0);
-        break;
-      }
-      break;
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_QUIT:
+            exit(0);
+            break;
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_o:
+                exit(0);
+                break;
+            }
+            break;
+        }
     }
-  }
 }
 
 void Dialogo::UpdateText() {
-  if (currentCharIndex < textLength) {
-    displayedText[currentCharIndex] = textToWrite[currentCharIndex];
-    currentCharIndex++;
-    displayedText[currentCharIndex] = '\0';
+    if (currentCharIndex < textLength) {
+        displayedText[currentCharIndex] = textToWrite[currentCharIndex];
+        currentCharIndex++;
+        displayedText[currentCharIndex] = '\0';
 
-    CreateTextTexture();
-  }
+        CreateTextTexture();
+    }
 }
 
 void Dialogo::Render() {
-  SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
-  SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+    SDL_RenderClear(renderer);
 
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderFillRect(renderer, &textBoxRect);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &textBoxRect);
 
-  if (TextTexture != NULL) {
-    SDL_Rect destRect = { textBoxRect.x + 10, textBoxRect.y + 10, TextSurface->w, TextSurface->h };
-    SDL_RenderCopy(renderer, TextTexture, NULL, &destRect);
-  }
+    if (TextTexture != nullptr) {
+        SDL_Rect destRect = { textBoxRect.x + 10, textBoxRect.y + 10, TextSurface->w, TextSurface->h };
+        SDL_RenderCopy(renderer, TextTexture, nullptr, &destRect);
+    }
 
-  SDL_RenderPresent(renderer);
+    SDL_RenderPresent(renderer);
 
-  SDL_Delay(delayAmount);
+    SDL_Delay(delayAmount);
 }
 
 void Dialogo::Clean() {
-  free(displayedText);
-  SDL_DestroyTexture(TextTexture);
-  SDL_FreeSurface(TextSurface);
-  TTF_CloseFont(font);
-  TTF_Quit();
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+    if (TextTexture != nullptr) {
+        SDL_DestroyTexture(TextTexture);
+        TextTexture = nullptr;
+    }
+
+    if (TextSurface != nullptr) {
+        SDL_FreeSurface(TextSurface);
+        TextSurface = nullptr;
+    }
+    TTF_CloseFont(font);
+    TTF_Quit();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 void Dialogo::Run() {
-  while (true) {
-    HandleEvents();
-    UpdateText();
-    Render();
-  }
+    while (true) {
+        HandleEvents();
+        UpdateText();
+        Render();
+    }
 }
--- FuMonoStudios/Game/src/Dialogo.h
+++ FuMonoStudios/Game/src/Dialogo.h
@@ -1,41 +1,44 @@
+// dialogo.h
+
 #ifndef DIALOGO_H
 #define DIALOGO_H
 
 #include <SDL.h>
 #include <SDL_ttf.h>
+#include <memory>
 
 class Dialogo {
 public:
-  Dialogo();
-  ~Dialogo();
+    Dialogo();
+    ~Dialogo();
 
-  void Run();
+    void Run();
 
 private:
-  SDL_Window* window;
-  SDL_Renderer* renderer;
-  TTF_Font* font;
-  SDL_Color textColor;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    TTF_Font* font;
+    SDL_Color textColor;
 
-  const char* textToWrite;
-  int textLength;
-  char* displayedText;
+    const char* textToWrite;
+    int textLength;
+    std::unique_ptr<char[]> displayedText;
 
-  SDL_Surface* TextSurface;
-  SDL_Texture* TextTexture;
+    SDL_Surface* TextSurface;
+    SDL_Texture* TextTexture;
 
-  SDL_Rect textBoxRect;
+    SDL_Rect textBoxRect;
 
-  int currentCharIndex;
-  int delayCounter;
-  int delayAmount;
+    int currentCharIndex;
+    int delayCounter;
+    int delayAmount;
 
-  void InitializeSDL();
-  void CreateTextTexture();
-  void HandleEvents();
-  void UpdateText();
-  void Render();
-  void Clean();
+    void InitializeSDL();
+    void CreateTextTexture();
+    void HandleEvents();
+    void UpdateText();
+    void Render();
+    void Clean();
 };
 
 #endif // DIALOGO_H
